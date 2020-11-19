6063db0cc56f0d5830e349178cb207b99a28d867
GitHub Action <action@github.com>
2020-11-19 05:43:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/sirwranwrap/tracetable/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/sirwranwrap/tracetable/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sirwranwrap.tracetable.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sirwranwrap.tracetable.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/sirwranwrap/tracetable/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sirwranwrap/tracetable/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sirwranwrap.tracetable.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sirwranwrap.tracetable.MyBundle
 
 class MyApplicationService {
 
